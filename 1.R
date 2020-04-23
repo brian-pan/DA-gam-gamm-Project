1ec420fc7903c32dac2aa9b83931db1ac5f9acd8
Brian Pan <brianpanzifan@gmail.com>
2020-04-23 14:57:07 -0400

    kable+plot

--- 1.R
+++ 1.R
@@ -22,3 +22,10 @@ mathGamInt = gam(
     Minority*Sex, 
   data=MathAchieve)
 mathGamInt$sp
+
+knitr::kable(
+  summary(mathGamInt)$p.table[,1:2], 
+  digits=1)
+
+plot(mathGamInt, select=1)
+plot(mathGamInt, select=2)
