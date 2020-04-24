ce2387d1ceb0721bf6391aae118076ffcff1fcae
Brian Pan <brianpanzifan@gmail.com>
2020-04-24 00:22:23 -0400

    delete itable

--- 1.R
+++ 1.R
@@ -61,9 +61,7 @@ mTable = mTable[,c(1,3)]
 
 colnames(mTable)=c('Country','mortality')
 
-iTable = readHTMLTable(getURL(
-  'https://en.wikipedia.org/wiki/List_of_countries_by_GDP_(PPP)_per_capita'
-), stringsAsFactors=FALSE,header=TRUE)s
+
 
 iTable = iTable[[5]]
 colnames(iTable) = gsub("([[:punct:]]|[[:space:]]).*", "", colnames(iTable))
