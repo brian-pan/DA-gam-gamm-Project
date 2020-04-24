7f597243e812cadecdc1a1ae0f79e4b8a28fceed
Brian Pan <brianpanzifan@gmail.com>
2020-04-24 11:43:19 -0400

    1

--- 1.R
+++ 1.R
@@ -56,3 +56,4 @@ mTable1 = XML::readHTMLTable(getURL(
   'https://en.wikipedia.org/wiki/List_of_countries_by_infant_mortality_rate'
 ), stringsAsFactors=FALSE, header=TRUE)
 mTable1 = mTable1[[which.max(unlist(lapply(mTable1, nrow)))]]
+mTable = mTable1[grep("^([[:digit:]]|[[:space:]])+$|^$|^Country|^World", mTable1[,1], invert=TRUE), ]
