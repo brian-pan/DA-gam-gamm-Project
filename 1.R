0522dd6212eabc3e645085a39c3d991731760c93
Brian Pan <brianpanzifan@gmail.com>
2020-05-10 20:00:45 -0400

    y_sim

--- 1.R
+++ 1.R
@@ -273,3 +273,11 @@ library('cowplot')
 set.seed(25)
 
 f2 <- function(x) 0.2 * x^11 * (10 * (1 - x))^6 + 10 * (10 * x)^3 * (1 - x)^10
+
+ysim <- function(n = 500, scale = 2) {
+  x <- runif(n)
+  e <- rnorm(n, 0, scale)
+  f <- f2(x)
+  y <- f + e
+  data.frame(y = y, x = x, f2 = f)
+}
